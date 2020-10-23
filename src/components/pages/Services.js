d5efa6c60e715946a38a5afb4c4c0d36ad0fc9e5
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-23 17:20:44 -0400

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,10 +2,10 @@ import React from 'react';
 import '../../App.css';
 
 export default function Services() {
-  return <>
+  return 
     <h1 className='services'>About Us</h1>;
   <div className='title'> VISION </div>
   <p>To be Amongst the top Indian leading players in Electric Two Wheelers by providing customers best of the electric mobility experiences through continuous research and development.</p>
   
-  </>
+  
 }
