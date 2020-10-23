af9b7cd3e96814ae3726b5b014eba99c2e2d6d34
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-23 17:18:52 -0400

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -4,5 +4,8 @@ import '../../App.css';
 export default function Services() {
   return <>
     <h1 className='services'>About Us</h1>;
+  <div className='title'> VISION </div>
+  <p>To be Amongst the top Indian leading players in Electric Two Wheelers by providing customers best of the electric mobility experiences through continuous research and development.</p>
+  
   </>
 }
