73c1332b41b55d289e4e4f6c505795bb11dc3fda
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-24 16:28:52 -0400

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -70,6 +70,56 @@ Developing Electric Vehicles will assist in energy conservation and security in 
         </div> 
     </div> 
 	  
+	  <div class="cards-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                    
+                    <div className="card">
+                        <span className="fa-stack">
+                            <span className="hexagon"></span>
+                            <i className="fas fa-binoculars fa-stack-1x"></i>
+                        </span>
+                        <div className="card-body">
+                            <h4 className="card-title">Environment Analysis</h4>
+                            <p>The starting point of any success story is knowing your current position in the business environment</p>
+                        </div>
+                    </div>
+                    
+
+                  
+                    <div className="card">
+                        <span className="fa-stack">
+                            <span className="hexagon"></span>
+                            <i className="fas fa-list-alt fa-stack-1x"></i>
+                        </span>
+                        <div className="card-body">
+                            <h4 className="card-title">Development Planning</h4>
+                            <p>After completing the environmental analysis the next stage is to design and  plan your development strategy</p>
+                        </div>
+                    </div>
+                    
+
+                   
+                    <div className="card">
+                        <span className="fa-stack">
+                            <span className="hexagon"></span>
+                            <i className="fas fa-chart-pie fa-stack-1x"></i>
+                        </span>
+                        <div className="card-body">
+                            <h4 className="card-title">Execution & Evaluation</h4>
+                            <p>In this phase you will focus on executing the actions plan and evaluating the results after each marketing campaign</p>
+                        </div>
+                    </div>
+                    
+
+                </div> 
+            </div> 
+        </div> 
+    </div> 
+    
+
 	  
   );   
 	  
