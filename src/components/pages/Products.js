41c3e69d9f9569cc2204a477b71855abc0f198af
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-24 18:34:37 -0400

    Update Products.js

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -2,7 +2,102 @@ import React from 'react';
 import '../../App.css';
 
 export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+  return 
+  <>
+    <div id="services" className="cards-2">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="section-title">SERVICES</div>
+                    <h2>Choose The Service Package<br> That Suits Your Needs</h2>
+                </div> 
+            </div> 
+            <div className="row">
+                <div className="col-lg-12">
+                    
+                    
+                    <div className="card">
+                        <div className="card-image">
+                            <img className="img-fluid" src="images/services-1.jpg" alt="alternative">
+                        </div>
+                        <div className="card-body">
+                            <h3 className="card-title">Off The Ground Off The Ground</h3>
+                            <p>Perfect for fresh ideas or young startups, this package will help get the business off the ground</p>
+                            <ul className="list-unstyled li-space-lg">
+                                <li className="media">
+                                    <i className="fas fa-square"></i>
+                                    <div className="media-body">Environment and competition</div>
+                                </li>
+                                <li className="media">
+                                    <i className="fas fa-square"></i>
+                                    <div className="media-body">Designing the marketing plan</div>
+                                </li>
+                            </ul>
+                            <p className="price">Starting at <span>$199</span></p>
+                        </div>
+                        <div className="button-container">
+                            <a className="btn-solid-reg page-scroll" href="#callMe">DETAILS</a>
+                        </div> <!-- end of button-container -->
+                    </div>
+                    <!-- end of card -->
 
+                    <!-- Card -->
+                    <div className="card">
+                        <div className="card-image">
+                            <img className="img-fluid" src="images/services-2.jpg" alt="alternative">
+                        </div>
+                        <div className="card-body">
+                            <h3 className="card-title">Accelerated Growth</h3>
+                            <p>Use this service pack to give your company the necessary impulse to become an industry leader</p>
+                            <ul className="list-unstyled li-space-lg">
+                                <li className="media">
+                                    <i className="fas fa-square"></i>
+                                    <div className="media-body">Business strategy planning</div>
+                                </li>
+                                <li className="media">
+                                    <i className="fas fa-square"></i>
+                                    <div className="media-body">Taking the planned actions</div>
+                                </li>
+                            </ul>
+                            <p className="price">Starting at <span>$299</span></p>
+                        </div>
+                        <div className="button-container">
+                            <a className="btn-solid-reg page-scroll" href="#callMe">DETAILS</a>
+                        </div> 
+                    </div>
+                    
+
+                    
+                    <div className="card">
+                        <div className="card-image">
+                            <img className="img-fluid" src="images/services-3.jpg" alt="alternative">
+                        </div>
+                        <div className="card-body">
+                            <h3 className="card-title">Market Domination</h3>
+                            <p>You already are a reference point in your industry now you need to learn about acquisitions</p>
+                            <ul className="list-unstyled li-space-lg">
+                                <li className="media">
+                                    <i className="fas fa-square"></i>
+                                    <div className="media-body">Maintaining the leader status</div>
+                                </li>
+                                <li className="media">
+                                    <i className="fas fa-square"></i>
+                                    <div className="media-body">Acquisitions the right way</div>
+                                </li>
+                            </ul>
+                            <p className="price">Starting at <span>$299</span></p>
+                        </div>
+                        <div className="button-container">
+                            <a className="btn-solid-reg page-scroll" href="#callMe">DETAILS</a>
+                        </div> 
+                    </div>
+                   
+
+                </div> 
+            </div> 
+        </div> 
+    </div> 
+  <h1 className='products'>PRODUCTS</h1>;
+</>
 }
 
