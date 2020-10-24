ee5f20ff781797375465f0d6ec48a3e2f80fc961
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-24 15:54:08 -0400

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -4,7 +4,7 @@ import '../../App.css';
 
 export default function Services() {
   return (
-	  
+/*	  
   <div className="head">
    <div className="boxabt"></div>
    <div className="header">
@@ -44,8 +44,33 @@ Developing Electric Vehicles will assist in energy conservation and security in 
 								</div>
 				</div>
 			</div>
+  */
   
-  
+	  
+	  
+	   
+    <div id="intro" className="basic-1">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-5">
+                    <div className="text-container">
+                        <div className="section-title">INTRO</div>
+                        <h2>We Offer Some Of The Best Business Growth Services In Town</h2>
+                        <p>Launching a new company or developing the market position of an existing one can be quite an overwhelming processs at times.</p>
+                        <p className="testimonial-text">"Our mission here at Aira is to get you through those tough moments relying on our team's expertise in starting and growing companies."</p>
+                        <div className="testimonial-author">Louise Donovan - CEO</div>
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+                <div className="col-lg-7">
+                    <div className="image-container">
+                        <img className="img-fluid" src="/images/image-1.jpg" alt="alternative">
+                    </div> <!-- end of image-container -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> 
+	  
+	  
   );   
 	  
   
