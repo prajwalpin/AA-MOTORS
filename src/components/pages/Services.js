53e60edc84bfdb54adb0f6c37899141bc1fcb32d
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-24 17:50:35 -0400

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../App.css';
-
+import './HeroSection.css';
 
 export default function Services() {
   return (
@@ -48,7 +48,9 @@ Developing Electric Vehicles will assist in energy conservation and security in 
   
 	  
 	<>  
-	   
+<div className='hero-container'>
+      <video src='/videos/video-5.mp4' autoPlay loop muted />
+	  
     <div id="intro" className="basic-1">
         <div className="container">
             <div className="row">
