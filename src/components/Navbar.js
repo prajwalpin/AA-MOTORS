64df5979d41b8a2e8cc77cd9448def71cb14a0c0
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-27 09:22:42 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,7 +68,7 @@ function Navbar() {
                 className='nav-links-mobile'
                 onClick={closeMobileMenu}
               >
-                Sign Up
+                Contact Us
               </Link>
             </li>
           </ul>
