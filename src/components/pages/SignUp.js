00bc0e3f45f6a68f580bf6bcf7492fd0bbcc1f16
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-27 07:57:11 -0400

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return (
+    <>
+     <HeroSection />
+      <Cards />
+      <Footer />
+    
+    </>
+    );
 }
