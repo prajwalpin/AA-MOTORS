a18d5418559127b86047b71086e5c6f0f41cf69e
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-28 16:20:46 -0400

    Update Products.js

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -27,7 +27,7 @@ export default function Products() {
                     <div className="card">
                         <div className="card-image">
                             
-                            <img className="cards__item__img" src="/images/KRITI-1.jpg" alt="alternative"></img>
+                            <img className="kriti" src="/images/KRITI-1.jpg" alt="alternative"></img>
                              
                         </div>
                      
