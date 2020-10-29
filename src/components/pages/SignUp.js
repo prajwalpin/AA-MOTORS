c893d92b7612db3ade8e9230a3f6dbcdc8235085
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-29 17:49:38 -0400

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -41,8 +41,8 @@ export default function SignUp() {
                                         <h3>Get in Touch</h3><br/>
                                         <h4>Please fill out this form and we will get in touch with you</h4><br/>
                                       
-                                        <input type="text" placeholder="Enter Your Name" name="name" value="" className="contactplace" required></input><br/>
-                                        <input type="email" placeholder="Enter Your Email" name="email"  value="" className="contactemail" required></input><br/>
+                                        <input type="text" placeholder="Enter Your Name" name="name" className="contactplace" required></input><br/>
+                                        <input type="email" placeholder="Enter Your Email" name="email" className="contactemail" required></input><br/>
                                           </div>
                                        </div>
                                     </div>
